e4163143f03a2becf8f7811d421316b26e1d0955
ifansyarifuddin <ifan.syarifuddin@gmail.com>
2024-05-04 13:50:25 +0700

    first name

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import beranda from './screens/beranda';
+import pengaturan from './screens/pengaturan';
+import notifikasi from './screens/notifikasi';
+
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={beranda} 
+      options={{
+          tabBarLabel: 'Beranda',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}/>
+      <Tab.Screen name="Pengaturan" component={pengaturan} 
+      options={{
+          tabBarLabel: 'Notifikasi',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="cog" color={color} size={26} />
+          ),
+        }}/>
+      <Tab.Screen name="Notifikasi" component={notifikasi} options={{
+          tabBarLabel: 'Notifikasi',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={26} />
+          ),
+        }} />
+    </Tab.Navigator>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MyTabs} options={{headerShown:false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ src/screens/pengaturan.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View } from 'react-native'
+import React from 'react'
+
+const pengaturan = () => {
+  return (
+    <View>
+    </View>
+  )
+}
+
+export default pengaturan
+
+const styles = StyleSheet.create({})
